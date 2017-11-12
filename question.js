e798fdf5a409955b69cd38b8ea53553f3b4cd972
Victoria Meng <nights1ststar@protonmail.com>
2017-11-12 02:54:13 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,26 @@
-'use strict';
+class Question {
+  constructor(question) {
+    this.content = question;
+    this.save();
+  }
+
+  save () {
+    this.constructor._All.push(this);
+  }
+
+  static All() {
+    return this._All;
+  }
+
+  static Find (id) {
+    const index = id - 1;
+    return this.All()[index];
+  }
+
+  matchesID () {
+
+  }
+
+}
+
+Question._All = []
